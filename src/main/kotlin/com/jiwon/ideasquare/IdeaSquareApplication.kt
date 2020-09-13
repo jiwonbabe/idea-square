9ae7cf262da4f3f33368eec4419882b4e2b85f7b
ashley.jiwonee <ashley.jiwonee@kakaomobility.com>
2020-09-13 18:17:05 +0900

    Project setting

---
+++ src/main/kotlin/com/jiwon/ideasquare/IdeaSquareApplication.kt
@@ -0,0 +1,11 @@
+package com.jiwon.ideasquare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IdeaSquareApplication
+
+fun main(args: Array<String>) {
+    runApplication<IdeaSquareApplication>(*args)
+}
---
+++ src/test/kotlin/com/jiwon/ideasquare/IdeaSquareApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jiwon.ideasquare
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IdeaSquareApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
